2704159e21220aa4cc1c4fafce61bad5ae6bca54
Kelly Selden <kellyselden@gmail.com>
2015-02-06 09:58:22 -0500

    array and injected model support

--- addon/mixins/dirty-confirm-route.js
+++ addon/mixins/dirty-confirm-route.js
@@ -1,18 +1,41 @@
 import Ember from 'ember';
 
+var ABORT = 0;
+var ROLLBACK = 1;
+var BUBBLE = 2;
+
+function checkModelForDirty(model, dirtyMessage, transition, continueRollingBack) {
+  if (model.get('isDirty') && !model.get('isSaving')) {
+    if (!continueRollingBack &&
+        !confirm(dirtyMessage || 'Leaving this page will lose your changes. Are you sure?')) {
+      transition.abort();
+      return ABORT;
+    }
+    model.rollback();
+    return ROLLBACK;
+  }
+  return BUBBLE;
+}
+
 export default Ember.Mixin.create({
   actions: {
     willTransition: function(transition) {
-      var model = this.currentModel;
-      if (model.get('isDirty') && !model.get('isSaving')) {
-        if (!confirm(this.get('dirtyMessage') || 'Leaving this page will lose your changes. Are you sure?')) {
-          transition.abort();
-          return;
-        } else {
-          model.rollback();
+      var model = this.get('dirtyModel') || this.currentModel;
+      var dirtyMessage = this.get('dirtyMessage');
+      if (Object.prototype.toString.call(model) === '[object Array]') {
+        var continueRollingBack;
+        for (var i = 0; i < model.length; i++) {
+          switch (checkModelForDirty(model[i], dirtyMessage, transition, continueRollingBack)) {
+            case ABORT: return;
+            case ROLLBACK: continueRollingBack = true;
+          }
         }
+        return true;
       }
-      return true;
+      switch (checkModelForDirty(model, dirtyMessage, transition)) {
+        case ROLLBACK: return true;
+        case BUBBLE: return true;
+      }
     }
   }
 });
