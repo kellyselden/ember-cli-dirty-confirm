b2ff2b136ea39da27511ec7adb383d5c8d59ac8f
Kelly Selden <kellyselden@gmail.com>
2015-03-14 13:16:47 -0400

    new test

--- tests/unit/mixins/dirty-confirm-route-test.js
+++ tests/unit/mixins/dirty-confirm-route-test.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
-import DirtyConfirmRouteMixin from 'ember-cli-dirty-confirm/mixins/dirty-confirm-route';
+import DirtyConfirmRouteMixin from '../../../mixins/dirty-confirm-route';
+import { module, test } from 'qunit';
 
 module('DirtyConfirmRouteMixin');
 
 // Replace this with your real tests.
-test('it works', function() {
+test('it works', function(assert) {
   var DirtyConfirmRouteObject = Ember.Object.extend(DirtyConfirmRouteMixin);
   var subject = DirtyConfirmRouteObject.create();
-  ok(subject);
+  assert.ok(subject);
 });
