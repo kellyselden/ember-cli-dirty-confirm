2fa670a5ccbfaebfbce79eb6cef215716f6dda52
Kelly Selden <kellyselden@gmail.com>
2015-06-25 16:08:55 -0400

    willTransition needs a return value

--- addon/mixins/dirty-confirm-route.js
+++ addon/mixins/dirty-confirm-route.js
@@ -8,35 +8,37 @@ var BUBBLE = 2;
 export default Ember.Mixin.create({
   checkModelForDirty: checkModelForDirty,
   isDirtyConfirmEnabled: true,
+
+  checkDirtyModel: function(transition) {
+    if (!this.get('isDirtyConfirmEnabled')) {
+      return;
+    }
+
+    // dirtyModel can be used for a property that isn't the route's main model
+    var model = this.get('dirtyModel') || this.currentModel;
+    var dirtyMessage = this.get('dirtyMessage');
+
+    if (Object.prototype.toString.call(model) === '[object Array]') {
+      var continueRollingBack;
+      for (var i = 0; i < model.length; i++) {
+        switch (this.checkModelForDirty(model[i], dirtyMessage, transition, continueRollingBack)) {
+          case ABORT: return;
+          // if one model was chosen to rollback, roll them all back
+          case ROLLBACK: continueRollingBack = true;
+        }
+      }
+      return true;
+    }
+
+    switch (this.checkModelForDirty(model, dirtyMessage, transition)) {
+      case ROLLBACK: return true;
+      case BUBBLE: return true;
+    }
+  },
+
   actions: {
     willTransition: function(transition) {
-      this.send('checkModelForDirty', transition);
-    },
-    checkModelForDirty: function(transition) {
-      if (!this.get('isDirtyConfirmEnabled')) {
-        return;
-      }
-
-      // dirtyModel can be used for a property that isn't the route's main model
-      var model = this.get('dirtyModel') || this.currentModel;
-      var dirtyMessage = this.get('dirtyMessage');
-
-      if (Object.prototype.toString.call(model) === '[object Array]') {
-        var continueRollingBack;
-        for (var i = 0; i < model.length; i++) {
-          switch (this.checkModelForDirty(model[i], dirtyMessage, transition, continueRollingBack)) {
-            case ABORT: return;
-            // if one model was chosen to rollback, roll them all back
-            case ROLLBACK: continueRollingBack = true;
-          }
-        }
-        return true;
-      }
-
-      switch (this.checkModelForDirty(model, dirtyMessage, transition)) {
-        case ROLLBACK: return true;
-        case BUBBLE: return true;
-      }
+      return this.checkDirtyModel(transition);
     }
   }
 });
